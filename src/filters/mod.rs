191d91101730be8696bc412eb58a72785eb63869
Nathan Graule <solarliner@gmail.com>
2023-12-31 23:31:28 +0100

    feat: state space impl for linear models

--- src/filters/mod.rs
+++ src/filters/mod.rs
@@ -2,3 +2,4 @@ pub mod biquad;
 // pub mod ladder;
 // pub mod sallenkey;
 pub mod svf;
+mod statespace;
---
+++ src/filters/statespace.rs
@@ -0,0 +1,87 @@
+use nalgebra::{SMatrix, SVector};
+use num_traits::Zero;
+use crate::dsp::DSP;
+use crate::Scalar;
+
+#[derive(Debug, Copy, Clone)]
+pub struct StateSpace<T: nalgebra::Scalar, const IN: usize, const STATE: usize, const OUT: usize> {
+    pub a: SMatrix<T, STATE, IN>,
+    pub b: SMatrix<T, STATE, STATE>,
+    pub c: SMatrix<T, OUT, IN>,
+    pub d: SMatrix<T, OUT, STATE>,
+    state: SVector<T, STATE>,
+}
+
+impl<T: nalgebra::Scalar + Zero, const IN: usize, const STATE: usize, const OUT: usize> StateSpace<T, IN, STATE, OUT> {
+    pub fn zeros() -> Self {
+        Self {
+            a: SMatrix::zeros(),
+            b: SMatrix::zeros(),
+            c: SMatrix::zeros(),
+            d: SMatrix::zeros(),
+            state: SVector::zeros(),
+        }
+    }
+}
+
+impl<T: Copy + nalgebra::Scalar, const IN: usize, const STATE: usize, const OUT: usize> StateSpace<T, IN, STATE, OUT> {
+    pub fn update_matrices(&mut self, other: &Self) {
+        self.a = other.a;
+        self.b = other.b;
+        self.c = other.c;
+        self.d = other.d;
+    }
+}
+
+impl<T: Scalar, const IN: usize, const STATE: usize, const OUT: usize> DSP<IN, OUT> for StateSpace<T, IN, STATE, OUT> {
+    type Sample = T;
+
+    fn process(&mut self, x: [Self::Sample; IN]) -> [Self::Sample; OUT] {
+        let x = SVector::from(x);
+        let y = self.c * x + self.d * self.state;
+        self.state = self.a * x + self.b * self.state;
+        y.into()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use numeric_literals::replace_float_literals;
+    use crate::dsp::{DSPBlock, utils::{slice_to_mono_block, slice_to_mono_block_mut}};
+
+    use super::*;
+
+    struct RC<T: nalgebra::Scalar>(StateSpace<T, 1, 1, 1>);
+
+    impl<T: Scalar> DSP<1, 1> for RC<T> {
+        type Sample = T;
+
+        fn process(&mut self, x: [Self::Sample; 1]) -> [Self::Sample; 1] {
+            self.0.process(x)
+        }
+    }
+
+    impl<T: Scalar> RC<T> {
+        #[replace_float_literals(T::from_f64(literal))]
+        fn new(fc: T) -> Self {
+            let new = SMatrix::<_, 1, 1>::new;
+            Self(StateSpace {
+                a: new(1.0),
+                b: new(-(fc-2.0)/(fc+2.0)),
+                c: new(fc/(fc+2.0)),
+                d: new(-fc*(fc-2.0)/(fc+2.0).simd_powi(2) + fc/(fc+2.0)),
+                ..StateSpace::zeros()
+            })
+        }
+    }
+
+    #[test]
+    fn test_rc_filter() {
+        let mut filter = RC::new(0.25);
+        let mut input = [0.0; 1024];
+        let mut output = [0.0; 1024];
+        input[0] = 1.0;
+        filter.process_block(slice_to_mono_block(&input), slice_to_mono_block_mut(&mut output));
+        insta::assert_csv_snapshot!(&output as &[_], { "[]" => insta::rounded_redaction(3) });
+    }
+}
