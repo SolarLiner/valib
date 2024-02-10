11f150d34160e84ca8594bf74d6e2ec2faff7464
Nathan Graule <solarliner@gmail.com>
2024-02-11 00:12:27 +0100

    chore: resolve warnings in abrasive example

--- plugins/abrasive/src/main.rs
+++ plugins/abrasive/src/main.rs
@@ -1,6 +1,12 @@
-use abrasive::Abrasive;
-use nih_plug::wrapper::standalone::nih_export_standalone;
-
+#[cfg(not(feature = "example"))]
 fn main() {
+    use abrasive::Abrasive;
+    use nih_plug::wrapper::standalone::nih_export_standalone;
+
     nih_export_standalone::<Abrasive<{ abrasive::NUM_BANDS }>>();
+}
+
+#[cfg(feature = "example")]
+fn main() {
+    panic!("Use the example binary instead of the main one");
 }
