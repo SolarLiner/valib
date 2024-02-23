d173466c248a0251ca05a660f8280e72322e8bca
Nathan Graule <solarliner@gmail.com>
2024-02-23 22:45:55 +0100

    fix(filters): deactivate designer module

--- src/filters/biquad/mod.rs
+++ src/filters/biquad/mod.rs
@@ -10,6 +10,7 @@ use crate::{
     Scalar,
 };
 
+#[cfg(never)]
 pub mod design;
 
 /// Biquad struct in Transposed Direct Form II. Optionally, a [`Saturator`](crate::saturators::Saturator) instance can be used
@@ -268,4 +269,3 @@ mod tests {
         insta::assert_csv_snapshot!(&output as &[_], { "[]" => insta::rounded_redaction(4) });
     }
 }
-
