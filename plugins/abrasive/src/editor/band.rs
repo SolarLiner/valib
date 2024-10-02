465120eba60dca69c0f928e1be7ebaa09360de8b
Nathan Graule <solarliner@gmail.com>
2024-10-02 21:15:26 +0200

    feat(abrasive): turn band selector into a spinbox

--- plugins/abrasive/src/editor/band.rs
+++ plugins/abrasive/src/editor/band.rs
@@ -14,7 +14,8 @@ struct BandSelector {
 }
 
 enum BandSelectorEvent {
-    SetActive(usize),
+    Previous,
+    Next,
 }
 
 impl BandSelector {
@@ -30,32 +31,33 @@ impl BandSelector {
         }
         .build(cx, |cx| {
             let lens = BandSelector::ctrl.map(|ctrl| {
-                (0..ctrl.step_count().unwrap_or(0))
-                    .map(|i| ctrl.preview_normalized(i as _))
-                    .map(|n| ctrl.normalized_value_to_string(n, true))
-                    .collect::<Vec<_>>()
+                ctrl.normalized_value_to_string(ctrl.modulated_normalized_value(), false)
             });
-            Binding::new(
-                cx,
-                BandSelector::ctrl.map(|ctrl| ctrl.modulated_plain_value() as usize),
-                move |cx, sel_lens| {
-                    let selected = sel_lens.get(cx);
-                    let selected_lens = lens.index(selected);
-                    Dropdown::new(
-                        cx,
-                        move |cx| Label::new(cx, selected_lens),
-                        move |cx| {
-                            let dropdown_entity = cx.current();
-                            List::new(cx, lens, move |cx, i, item_lens| {
-                                Label::new(cx, item_lens).on_press(move |cx| {
-                                    cx.emit(BandSelectorEvent::SetActive(i));
-                                    cx.emit_to(dropdown_entity, Dropdown)
-                                });
-                            });
-                        },
-                    );
-                },
-            );
+            Spinbox::new(cx, lens, SpinboxKind::Horizontal, SpinboxIcons::Chevrons)
+                .on_increment(|cx| cx.emit(BandSelectorEvent::Next))
+                .on_decrement(|cx| cx.emit(BandSelectorEvent::Previous));
+            /*            Binding::new(
+                            cx,
+                            BandSelector::ctrl.map(|ctrl| ctrl.modulated_plain_value() as usize),
+                            move |cx, sel_lens| {
+                                let selected = sel_lens.get(cx);
+                                let selected_lens = lens.index(selected);
+                                Dropdown::new(
+                                    cx,
+                                    move |cx| Label::new(cx, selected_lens),
+                                    move |cx| {
+                                        let dropdown_entity = cx.current();
+                                        List::new(cx, lens, move |cx, i, item_lens| {
+                                            Label::new(cx, item_lens).on_press(move |cx| {
+                                                cx.emit(BandSelectorEvent::SetActive(i));
+                                                cx.emit_to(dropdown_entity, Dropdown)
+                                            });
+                                        });
+                                    },
+                                );
+                            },
+                        );
+            */
         })
     }
 }
@@ -67,10 +69,18 @@ impl View for BandSelector {
 
     fn event(&mut self, cx: &mut EventContext, event: &mut Event) {
         event.map(|ev, _| match ev {
-            &BandSelectorEvent::SetActive(i) => {
+            BandSelectorEvent::Previous => {
                 self.ctrl.begin_set_parameter(cx);
-                self.ctrl
-                    .set_normalized_value(cx, self.ctrl.preview_normalized(i as _));
+                let current = self.ctrl.unmodulated_plain_value();
+                let normalized = self.ctrl.preview_normalized(current - 1.0);
+                self.ctrl.set_normalized_value(cx, normalized);
+                self.ctrl.end_set_parameter(cx);
+            }
+            BandSelectorEvent::Next => {
+                self.ctrl.begin_set_parameter(cx);
+                let current = self.ctrl.unmodulated_plain_value();
+                let normalized = self.ctrl.preview_normalized(current + 1.0);
+                self.ctrl.set_normalized_value(cx, normalized);
                 self.ctrl.end_set_parameter(cx);
             }
         })
